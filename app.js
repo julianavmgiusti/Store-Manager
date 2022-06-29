69f0a6d75c45e6653181c5fe52f94989f089a5b4
Juliana Giusti <juliana.vmelo@gmail.com>
2022-06-29 14:34:39 -0300

    opening PR

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 app.get('/', (_request, response) => {
   response.send();
 });
-
+// abrindo pr
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
